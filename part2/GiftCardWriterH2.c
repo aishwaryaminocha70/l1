dbf956d1f3bef4c3ce85a98882a8912ac6e1d456
acy238 <78458967+acy238@users.noreply.github.com>
2021-02-12 14:59:49 -0800

    Update GiftCardWriterH2.c

--- part2/GiftCardWriterH2.c
+++ part2/GiftCardWriterH2.c
@@ -14,18 +14,18 @@
 
 struct this_gift_card examplegc;
 struct gift_card_data examplegcd;
-struct gift_card_record_data examplegcrd;
-struct gift_card_amount_change examplegcac;
+//struct gift_card_record_data examplegcrd;
+//struct gift_card_amount_change examplegcac;
 
 
 // Break it up so that we don't have long functions!  Good programming style!
 //  (JAC: This is so wrong.  Global variable use / initialization is a 
 //  terrible thing to do.)
 void setupgc() {
-	examplegc.num_bytes = 72;
+	examplegc.num_bytes = 8;
 	examplegc.gift_card_data = (void *) &examplegcd;
-	examplegcd.merchant_id = "GiftCardz.com                   ";
-	examplegcd.customer_id = "DuaneGreenes Store 1451         ";
+	//examplegcd.merchant_id = "GiftCardz.com                   ";
+	//examplegcd.customer_id = "DuaneGreenes Store 1451         ";
 	examplegcd.number_of_gift_card_records = 0;
 
 	/* JAC: Something seems fishy... */
@@ -47,10 +47,10 @@ void setupgc() {
 void writegc() {
 	FILE *fd1;
 	// JAC: Why don't any of these check for error return codes?!?
-	fd1 = fopen("crash2.gft","w");
+	fd1 = fopen("crash2v3.gft","w");
 	fwrite(&examplegc.num_bytes,4,1,fd1);
-	fwrite(examplegcd.merchant_id,32,1,fd1);
-	fwrite(examplegcd.customer_id,32,1,fd1);
+	//fwrite(examplegcd.merchant_id,32,1,fd1);
+	//fwrite(examplegcd.customer_id,32,1,fd1);
 	fwrite(&examplegcd.number_of_gift_card_records,4,1,fd1);
 	//fwrite(&examplegcrd.record_size_in_bytes,4,1,fd1);
 	//fwrite(&examplegcrd.type_of_record,4,1,fd1);
