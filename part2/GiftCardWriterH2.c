56f868bd51ceae9c9f6b74a5e21e008d972a8044
acy238 <78458967+acy238@users.noreply.github.com>
2021-02-13 16:14:08 -0800

    Update for crash2 gift card - empty giftcard

--- part2/GiftCardWriterH2.c
+++ part2/GiftCardWriterH2.c
@@ -13,7 +13,7 @@
 #include <time.h>
 
 struct this_gift_card examplegc;
-struct gift_card_data examplegcd;
+//struct gift_card_data examplegcd;
 //struct gift_card_record_data examplegcrd;
 //struct gift_card_amount_change examplegcac;
 
@@ -22,11 +22,11 @@ struct gift_card_data examplegcd;
 //  (JAC: This is so wrong.  Global variable use / initialization is a 
 //  terrible thing to do.)
 void setupgc() {
-	examplegc.num_bytes = 8;
-	examplegc.gift_card_data = (void *) &examplegcd;
+	//examplegc.num_bytes = 0;
+	//examplegc.gift_card_data = (void *) &examplegcd;
 	//examplegcd.merchant_id = "GiftCardz.com                   ";
 	//examplegcd.customer_id = "DuaneGreenes Store 1451         ";
-	examplegcd.number_of_gift_card_records = 0;
+	//examplegcd.number_of_gift_card_records = 0;
 
 	/* JAC: Something seems fishy... */
 	//examplegcd.gift_card_record_data = malloc(examplegcd.number_of_gift_card_records);
@@ -47,11 +47,11 @@ void setupgc() {
 void writegc() {
 	FILE *fd1;
 	// JAC: Why don't any of these check for error return codes?!?
-	fd1 = fopen("crash2v3.gft","w");
-	fwrite(&examplegc.num_bytes,4,1,fd1);
+	fd1 = fopen("crash2emptyv2.gft","w");
+	//fwrite(&examplegc.num_bytes,4,1,fd1);
 	//fwrite(examplegcd.merchant_id,32,1,fd1);
 	//fwrite(examplegcd.customer_id,32,1,fd1);
-	fwrite(&examplegcd.number_of_gift_card_records,4,1,fd1);
+	//fwrite(&examplegcd.number_of_gift_card_records,4,1,fd1);
 	//fwrite(&examplegcrd.record_size_in_bytes,4,1,fd1);
 	//fwrite(&examplegcrd.type_of_record,4,1,fd1);
 	//fwrite(&examplegcac.amount_added,4,1,fd1);
