990847f90ea0892307772ec82eb73e1bfea52f80
acy238 <78458967+acy238@users.noreply.github.com>
2021-02-14 16:56:28 -0800

    Add gift card for crash 2 test

---
+++ part2/crash2writer.c
@@ -0,0 +1,98 @@
+/*
+ * Gift Card Example Writer
+ * Original Author: Shoddycorp's Cut-Rate Contracting
+ * Maintainer: ???
+ * Comments added by Justin Cappos (JAC)
+ * Modification Date: 8 July 2020
+ */
+
+#include <stdio.h>
+#include "giftcard.h"
+
+/* JAC: Why is this included? */
+#include <time.h>
+
+struct this_gift_card examplegc;
+struct gift_card_data examplegcd;
+struct gift_card_record_data examplegcrd;
+// struct gift_card_amount_change examplegcac;
+struct gift_card_program examplegcp;
+
+struct gift_card_record_data examplegcrd2;
+//struct gift_card_amount_change examplegcac2;
+struct gift_card_program examplegcp2;
+
+
+// Break it up so that we don't have long functions!  Good programming style!
+//  (JAC: This is so wrong.  Global variable use / initialization is a 
+//  terrible thing to do.)
+void setupgc() {
+	examplegc.num_bytes = 664;
+	examplegc.gift_card_data = (void *) &examplegcd;
+	examplegcd.merchant_id = "CCCCCCCC1.com                   ";
+	examplegcd.customer_id = "DuaneGreenes Cncin cte9         ";
+	//examplegcd.number_of_gift_card_records = 777 and type 2 record try
+	examplegcd.number_of_gift_card_records = 777;
+
+	/* JAC: Something seems fishy... */
+	examplegcd.gift_card_record_data = malloc(examplegcd.number_of_gift_card_records);
+	/* JAC: here too! */
+	examplegcd.gift_card_record_data[0] = (void *) &examplegcrd;
+	examplegcrd.record_size_in_bytes = 44;
+	examplegcrd.type_of_record = 2; // JAC: Should be enum!  amount_change
+	examplegcrd.actual_record = (void *) &examplegcp;
+	examplegcp.message = "This is crash one for the testing - say hello";
+	unsigned char uc = 9;
+	examplegcp.program = &uc;
+	
+	
+	
+	examplegcd.gift_card_record_data[1] = (void *) &examplegcrd2;
+	examplegcrd2.record_size_in_bytes = 44;
+	examplegcrd2.type_of_record = 2; // JAC: Should be enum!  amount_change
+	examplegcrd2.actual_record = (void *) &examplegcp2;
+	examplegcp2.message = "Hello Hang1";
+	// this below cannot be an int, find another option and fix
+	// examplegcp.program = '11';
+	unsigned char uc2 = 9;
+	examplegcp2.program = &uc2;		
+	
+	
+}
+
+
+// moved into separate files to avoid erroenous style checker error...
+/* JAC: opening and writing a fixed filename using a global is terrible style.
+ *     the style checker was right!  This code is terrible   
+ */
+void writegc() {
+	FILE *fd1;
+	// JAC: Why don't any of these check for error return codes?!?
+	fd1 = fopen("crash2.gft","w");
+	fwrite(&examplegc.num_bytes,4,1,fd1);
+	fwrite(examplegcd.merchant_id,32,1,fd1);
+	fwrite(examplegcd.customer_id,32,1,fd1);
+	fwrite(&examplegcd.number_of_gift_card_records,4,1,fd1);
+	
+	
+	fwrite(&examplegcrd.record_size_in_bytes,4,1,fd1);
+	fwrite(&examplegcrd.type_of_record,4,1,fd1);
+	fwrite(examplegcp.message,32,1,fd1);
+	fwrite(examplegcp.program,256,1,fd1);
+	// fwrite(examplegcp.message,32,1,fd1);
+	
+	fwrite(&examplegcrd2.record_size_in_bytes,4,1,fd1);
+	fwrite(&examplegcrd2.type_of_record,4,1,fd1);
+	fwrite(examplegcp2.message,32,1,fd1);
+	fwrite(examplegcp2.program,256,1,fd1);
+	
+	fclose(fd1);
+}
+
+/* JAC: No args and return -1 for no reason!?! */
+int main(void) {
+
+	setupgc();
+	writegc();
+	return -1;
+}
