602a7a225dcea61daf9d410f793b8cfa9ae48b82
acy238 <78458967+acy238@users.noreply.github.com>
2021-02-27 18:13:09 -0800

    Update giftcardreader2.c

--- part2/giftcardreader2.c
+++ part2/giftcardreader2.c
@@ -191,7 +191,7 @@ struct this_gift_card *gift_card_reader(FILE *input_fd) {
 		struct gift_card_data *gcd_ptr;
 		/* JAC: Why aren't return types checked? */
 		fread(&ret_val->num_bytes, 4,1, input_fd);
-		if (ret_val->num_bytes < 0) {
+		if (ret_val->num_bytes <= 0) {
 			return ret_val;
 		}
 		
